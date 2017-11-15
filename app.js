bee909dfdec046c034fd38990205ab54dfa8f24f
LittleJono <jtem977@aucklanduni.ac.nz>
2017-11-15 22:48:35 +1300

    first version

--- app.js
+++ app.js
@@ -3,19 +3,192 @@ const client = new Discord.Client();
 const config = require('config');
 const token = config.get('discord-token');
 const apikey = config.get('destiny-api-key');
+const request = require('request');
+const fs = require('fs');
+const async = require('async');
+
+var listOfUsers = []
+
+fs.readFile('users.txt', (err, data) => {
+    if (err) throw err;
+    listOfUsers = data.toString('utf8').split("\r\n");
+    console.log(listOfUsers)
+});
+
+
+
+
+async function getNewUser(user, usersliced, channel) {
+    var result = await doesUserExist(user, usersliced);
+    if (result == 1) {
+        listOfUsers.push(usersliced);
+        fs.appendFile('users.txt', '\r\n' + usersliced, (err) => {
+            if (err) throw err;
+            console.log(usersliced + " was added to the list.");
+        });
+        var output = await getUserData(usersliced);
+        printStats(user, channel);
+    } else {
+        channel.send("User not found.");
+    }
+}
+
+function getUserData(user) {
+    var promise = new Promise(function(resolve, reject) {
+        fs.readFile('users/' + user, (err, data) => {
+            if (err) throw err;
+            data = JSON.parse(data);
+            var options = {
+                url: 'https://www.bungie.net/platform/Destiny2/4/Account/' + data['destinyMembershipId'] + '/Stats/',
+                headers: {
+                    'X-API-Key': apikey
+                }
+            };
+            request(options, function(error, response, body) {
+                data["data"] = JSON.parse(body)["Response"];
+                fs.writeFile("users/" + user, JSON.stringify(data, null, 4), (err) => {
+                    if (err) throw err;
+                    resolve(1);
+                });
+            })
+        })
+    })
+}
+
+
+function printStats(user, channel) {
+    fs.readFile('users/' + user, (err, data) => {
+        data = JSON.parse(data);
+        
+        channel.send({
+            embed: {
+                color: 3447003,
+                title: (user.charAt(0).toUpperCase() + user.slice(1) + "'s PVP Stats"),
+                description: "These stats are from all characters combined.",
+                fields: [{
+                    name: "Kills",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.kills.basic.value
+                }, {
+                    name: "Assists",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.assists.basic.value
+                }, {
+                    name: "Deaths",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.deaths.basic.value
+                }, {
+                    name: "K/D",
+                    value: (data.data.mergedAllCharacters.results.allPvP.allTime.kills.basic.value / data.data.mergedAllCharacters.results.allPvP.allTime.deaths.basic.value).toFixed(3)
+                }, {
+                    name: "Headshot Kills",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.precisionKills.basic.value
+                }, {
+                    name: "Revives",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.resurrectionsPerformed.basic.value
+                }, {
+                    name: "Suicides",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.suicides.basic.value
+                }, {
+                    name: "W/L ratio",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.winLossRatio.basic.displayValue
+                }, {
+                    name: "Longest Killing Spree",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.longestKillSpree.basic.value
+                }, {
+                    name: "Highest Light Level",
+                    value: data.data.mergedAllCharacters.results.allPvP.allTime.highestLightLevel.basic.value
+                }]
+            }
+        });
+    });
+}
+
+function doesUserExist(user, usersliced) {
+    return new Promise(function(resolve, reject) {
+        var options = {
+            url: 'https://www.bungie.net/Platform/User/SearchUsers/?q=' + user,
+            headers: {
+                'X-API-Key': apikey
+            }
+        };
+        request(options, function(error, response, body) {
+            body = JSON.parse(body)
+            results = body["Response"]
+            if (results.length == 0) {
+                resolve(0)
+            }
+            for (i in results) {
+                try {
+                    if (results[i]["blizzardDisplayName"].toLowerCase() == user) {
+                        var newUser = {}
+                        newUser["battletag"] = user
+                        newUser["membershipId"] = results[i]["membershipId"];
+
+                        var options = {
+                            url: 'https://www.bungie.net/Platform/User/GetMembershipsById/' + results[i]["membershipId"] + '/-1/',
+                            headers: {
+                                'X-API-Key': apikey
+                            }
+                        };
+                        request(options, function(error, response, body) {
+                            body = JSON.parse(body);
+                            newUser["destinyMembershipId"] = body.Response.destinyMemberships[0].membershipId;
+
+                            fs.writeFile("users/" + usersliced, JSON.stringify(newUser, null, 4), (err) => {
+                                if (err) throw err;
+                            });
+                            resolve(1);
+                        });
+
+                    } else {
+                        if (i == results.length - 1) {
+                            resolve(0);
+                        }
+                    }
+                } catch (err) {
+                	if (i == results.length - 1) {
+                         resolve(0);
+                        }
+                }
+            };
+        });
+    });
+};
+
+var counter = 0;
+
+setInterval(function() {
+    getUserData(listOfUsers[counter]);
+    counter += 1;
+    if (counter == listOfUsers.length) {
+        counter = 0;
+    }
+}, 5000);
+
+
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-	console.log(msg.content.slice(0,18))
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-  if (msg.content.slice(0,17).toLowerCase() === '.getdestinystats ') {
-    msg.channel.send('Querying: ' + msg.content.slice(17));
-  }
+    if (msg.content.slice(0, 17).toLowerCase() === '.getdestinystats ') {
+        var name = msg.content.slice(17).toLowerCase();
+        if (name.indexOf('#') == -1) {
+            nameSliced = name
+        } else {
+            nameSliced = name.slice(0, name.indexOf('#'));
+        }
+        position = listOfUsers.indexOf(nameSliced);
+        if (position == -1) {
+            if (name.indexOf('#') == -1) {
+                msg.channel.send("This user hasn't been searched before, please include the battletag (E.g. 'Jono#1234')", name, nameSliced);
+            } else {
+                console.log("getting new user")
+                getNewUser(name, nameSliced, msg.channel)
+            }
+        } else {
+            printStats(nameSliced, msg.channel)
+        }
+    }
 
 });
 
